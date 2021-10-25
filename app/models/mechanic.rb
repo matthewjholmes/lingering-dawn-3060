6d7acc6c39e261b96e0d7f0c5ed4d82b4ffff5c2
matthewjholmes <ilookup@gmail.com>
2021-10-25 09:48:03 -0600

    Implement mechanic average years experience method

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -2,4 +2,7 @@ class Mechanic < ApplicationRecord
   has_many :mechanic_rides
   has_many :rides, through: :mechanic_rides
 
+  def self.average_years_experience
+    average(:years_experience)
+  end
 end
--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -6,4 +6,13 @@ RSpec.describe Mechanic do
     it { should have_many(:rides).through(:mechanic_rides) }
   end
 
+  describe 'model methods' do
+    it 'calcs average years_experience' do
+      mechanic1 = Mechanic.create!(name: 'Mechanic 1', years_experience: 1)
+      mechanic2 = Mechanic.create!(name: 'Mechanic 2', years_experience: 2)
+      mechanic3 = Mechanic.create!(name: 'Mechanic 3', years_experience: 3)
+
+      expect(Mechanic.average_years_experience).to eq(2)
+    end
+  end
 end
